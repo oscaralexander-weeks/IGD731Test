a1955de0a6ba84e501201a30c3f5f8268cb413d4
oscaralexander-weeks <oscarweeks@icloud.com>
2025-06-11 21:49:09 +0100

    Basic Inventory System Set Up
    slight issue with triggering the item pickup twice

---
+++ Assets/GameFolder/UI/Inventory/Items/HealthPotionItem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPotionItem : ItemBaseClass
+{
+    [SerializeField] private FloatVariable HP;
+    public FloatReference amount;
+    public override void Use()
+    {
+        if(stackSize.Value > 0 && HP.Value < 100)
+        {
+            HP.ApplyChange(amount);
+            stackSize.ApplyChange(-1);
+            Remove();
+        }
+    }
+
+    
+}
