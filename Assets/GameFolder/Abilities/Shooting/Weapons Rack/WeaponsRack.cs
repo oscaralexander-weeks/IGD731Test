ccb58dbd89a2be63d6857f00ddbe767ad81f85c7
oscaralexander-weeks <oscarweeks@icloud.com>
2025-06-27 17:57:25 +0100

    Added Cooldown Timer to Weapon Rack

--- Assets/GameFolder/Abilities/Shooting/Weapons Rack/WeaponsRack.cs
+++ Assets/GameFolder/Abilities/Shooting/Weapons Rack/WeaponsRack.cs
@@ -8,18 +8,38 @@ public class WeaponsRack : MonoBehaviour
     public List<BaseDefaultWeapon> Weapons = new List<BaseDefaultWeapon>();
     [SerializeField] private IconPanelUIAbility weaponUI;
 
+    public bool IsOnCooldown;
+    public float CooldownTimer;
+    public float Cooldown;
+
+    private void Update()
+    {
+        if (IsOnCooldown)
+        {
+            Cooldown -= Time.deltaTime;
+            if(Cooldown < 0.01f)
+            {
+                Cooldown = CooldownTimer;
+            }
+        }
+    }
+
     public void SwitchWeapon(List<BaseDefaultWeapon> playerWeapons)
     {
-        playerWeapons[0] = Weapons[0];
-        playerWeapons[0].AbilityCount = playerWeapons[0].AbilityCountMax;
-        if(weaponUI!= null)
+        if (!IsOnCooldown)
         {
-            weaponUI.Ability = playerWeapons[0];
-        }
+            playerWeapons[0] = Weapons[0];
+            playerWeapons[0].AbilityCount = playerWeapons[0].AbilityCountMax;
+            if(weaponUI!= null)
+            {
+                weaponUI.Ability = playerWeapons[0];
+            }
 
-        if(weaponUI == null)
-        {
-            Debug.LogWarning("icon not assigned in inspector");
+            if(weaponUI == null)
+            {
+                Debug.LogWarning("icon not assigned in inspector");
+            }
+            IsOnCooldown = true;
         }
     }
 
