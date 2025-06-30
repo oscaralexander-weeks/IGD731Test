fe9977c3b50e7e61c79c0223c253f38a9638d630
oscaralexander-weeks <oscarweeks@icloud.com>
2025-06-30 22:53:00 +0100

    Basic BTree for Patrol Variant
    need to optimise and add additional nodes but works in practice

---
+++ Assets/GameFolder/Enemy/EnemyVariants/Enemy Variant AI/CheckPlayerInRangeRay.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourTree;
+
+public class CheckPlayerInRangeRay : Node 
+{
+    private Transform _transform;
+    public CheckPlayerInRangeRay(Transform transform)
+    {
+        _transform = transform;
+    }
+
+
+    public override NodeState Evaluate()
+    {
+        object t = GetData("target");
+        
+        if(t == null)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(_transform.position, _transform.forward, out hit, 50f))
+            {
+                Debug.Log("hit");
+                parent.parent.SetData("target", hit.transform);
+                state = NodeState.SUCCESS;
+                return state;
+            }
+
+        }
+
+        if(t != null)
+        {
+            state = NodeState.SUCCESS;
+            return state;
+        }
+
+
+        state = NodeState.FAILURE;
+        return state;
+    }
+}
---
+++ Assets/GameFolder/Enemy/EnemyVariants/Enemy Variant AI/PatrolBTVariant.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviourTree;
+
+public class PatrolBTVariant : BTree
+{
+    public Transform[] waypoints;
+    public static float speed = 3f;
+    public static float fovRange = 6f;
+
+    //additions I've made specific to this game 
+    private Enemy _enemy;
+
+    private void Awake()
+    {
+        _enemy = GetComponent<Enemy>();
+    }
+
+    protected override Node SetUpTree()
+    {
+        Node root = new Selector(new List<Node>
+        {
+            new BehaviourTree.Sequence(new List<Node>
+        {
+            new CheckPlayerInRangeRay(transform),
+            new TaskGoToTarget(transform)
+        }),
+            new TaskPatrol(transform, waypoints)
+        });
+
+        return root;
+    }
+}
