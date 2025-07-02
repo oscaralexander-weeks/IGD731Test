f8185e81ac2f4bfb750b126b572d167dc34c4c43
oscaralexander-weeks <oscarweeks@icloud.com>
2025-07-02 22:31:37 +0100

    changed layer collisions

--- Assets/GameFolder/Enemy/EnemyProjectileBaseClass.cs
+++ Assets/GameFolder/Enemy/EnemyProjectileBaseClass.cs
@@ -19,7 +19,7 @@ public class EnemyProjectileBaseClass : MonoBehaviour
     private void OnEnable()
     {
         _returnToPoolTimerCoroutine = StartCoroutine(ReturnToPoolAfterTime());
-        Physics.IgnoreLayerCollision(0, 7);
+        Physics.IgnoreLayerCollision(0, 8);
     }
 
     private void OnCollisionEnter(Collision collision)
