39fe76c594788aa477c0b0cb11e49dd93803f0d6
oscaralexander-weeks <oscarweeks@icloud.com>
2025-07-07 23:26:00 +0100

    Stage Clear Breakdown Change
    Added Style Scoring

--- Assets/GameFolder/UI/GameEnd/StageClearBreakdown.cs
+++ Assets/GameFolder/UI/GameEnd/StageClearBreakdown.cs
@@ -8,10 +8,27 @@ public class StageClearBreakdown : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI StyleScoreTextComponent, StatsTextComponent;
     [SerializeField] private StageStats stageStats;
-
+    private float _style;
     public void CheckStageStats()
     {
         StatsTextComponent.text = ($"Hits: {stageStats.Hits.Value} \n Misses: {stageStats.Misses.Value} \n Entertainment: {stageStats.Entertainment.Value}");
+        CheckStyle(stageStats.Style);
     }
 
+
+    private void CheckStyle(FloatVariable style)
+    {
+        float v = style.Value;
+        StyleScoreTextComponent.text = v switch
+        {
+            < 15 => "Style: F",
+            < 30 => "Style: E",
+            < 45 => "Style: D",
+            < 60 => "Style: C",
+            < 75 => "Style: B",
+            < 90 => "Style: A",
+            < 150 => "Style: S",
+            _ => StyleScoreTextComponent.text // fallback
+        };
+    }
 }
